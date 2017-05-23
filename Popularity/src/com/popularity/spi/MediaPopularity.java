b1fc91cd01e634ea65a63064f72bd244c1156121
hiba <hiba.enis@gmail.com>
2017-05-23 02:00:27 +0200

    Update SPI

--- Popularity/src/com/popularity/spi/MediaPopularity.java
+++ Popularity/src/com/popularity/spi/MediaPopularity.java
@@ -5,9 +5,15 @@ import com.popularity.media.metadata.MediaMetadataMetrics;
 import com.popularity.user.metrcis.UserPopularityMetrics;
 
 public interface MediaPopularity {
-
+//For identifying the appropriate provider
 	public String getProviderName();
+	
+	// service for encapsulate the metadata metrics of the media entity (i.e. Text, Photo and Video) correpond to the id_media
 	public MediaMetadataMetrics getmetadata (String id_media);
+	
+	// service for encapsulate the feedback of users in front of the target media entity (i.e. Text, Photo and Video) correpond to the id_media
 	public MediaUserFeedBackMetrics getUserFeeBack (String id_media);
+	
+	// service for encapsulate the metrics of the author of the media entity (i.e. Text, Photo and Video) correpond to the id_media
 	public UserPopularityMetrics getAuthorPopularity (String id_media);
 }
--- Popularity/src/com/popularity/spi/UserPopularity.java
+++ Popularity/src/com/popularity/spi/UserPopularity.java
@@ -7,9 +7,16 @@ import com.popularity.user.metrcis.UserProfileMetadata;
 
 public interface UserPopularity {
 	
+	// service for encapsulate the metadata metrics of the user entity 
 public UserProfileMetadata getMetadata(String names);
+
+// service for encapsulate the  metrics describes the activities of the user entity 
 public UserActivitiesMetadata getActivities(String name);
+
+// service for encapsulate the metrics of the connectivity  of the user entity 
 public UserConnectivityMetadata getConnectivity(String name);
+
+// service for encapsulate all the metrics of the user popularity 
 public UserPopularityMetrics getPopularity (String name);
 	
 }
